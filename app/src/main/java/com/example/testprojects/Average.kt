9ddbf5b7483d87c99a029642102d74435263975f
Chris-Eminence <emichris380@gmail.com>
2020-08-24 13:53:39 -0700

    Average of two numbers with kotln

--- app/src/main/java/com/example/testprojects/Average.kt
+++ app/src/main/java/com/example/testprojects/Average.kt
@@ -1,2 +1,23 @@
 package com.example.testprojects
 
+import java.util.*
+
+class Average{
+    fun average(){
+        val reader = Scanner (System.`in`)
+
+        println("Enter your first number")
+        var firstNumber = reader.nextInt()
+
+        println("Enter your second number")
+        var secondNumber = reader.nextInt()
+
+        var result = (firstNumber + secondNumber) / 2
+        println("Your Average number is $result")
+    }
+}
+
+fun main(){
+    val obj = Average()
+    obj.average()
+}
