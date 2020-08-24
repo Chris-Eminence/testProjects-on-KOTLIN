a2576a965101f3c37f661c80f28d9259e55374f2
Chris-Eminence <emichris380@gmail.com>
2020-08-24 16:16:38 -0700

    reversed String

--- app/src/main/java/com/example/testprojects/reversedString.kt
+++ app/src/main/java/com/example/testprojects/reversedString.kt
@@ -1,2 +1,16 @@
 package com.example.testprojects
 
+class Reversed{
+    fun rev(){
+        println("Type a word you want to reverse")
+        val word = readLine()!!
+        val reverse = word.reversed()
+        print(reverse)
+    }
+}
+
+fun main(){
+    val ttt = Reversed()
+        ttt.rev()
+
+}
