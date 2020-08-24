3d86a99205adf8af43b0c1f3df46e89a9dcc7b26
Chris-Eminence <emichris380@gmail.com>
2020-08-24 13:49:45 -0700

    Division with KOTLIN

--- app/src/main/java/com/example/testprojects/division.kt
+++ app/src/main/java/com/example/testprojects/division.kt
@@ -1,1 +1,23 @@
 package com.example.testprojects
+
+import java.util.Scanner
+
+class Division{
+    fun divide(){
+        val reader = Scanner (System.`in`)
+
+        println("Enter your first number")
+        var firstNumber = reader.nextInt()
+
+        println("Enter your second number")
+        var secondNumber = reader.nextInt()
+
+        var result = firstNumber / secondNumber
+        println("Your divided result is $result")
+    }
+}
+
+fun main(){
+    val obj = Division()
+    obj.divide()
+}
