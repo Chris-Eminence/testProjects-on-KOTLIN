2b737b0cf3f9e63ded889cea127a6475a1158bed
Chris-Eminence <emichris380@gmail.com>
2020-08-24 13:45:52 -0700

    Multiplication with kotlin

--- app/src/main/java/com/example/testprojects/multiplication.kt
+++ app/src/main/java/com/example/testprojects/multiplication.kt
@@ -1,5 +1,22 @@
 package com.example.testprojects
+import java.util.Scanner
 
 class Multiplication{
+    fun multi(){
+        val reader = Scanner (System.`in`)
 
+        println("Enter your first number")
+        var firstNumber = reader.nextInt()
+
+        println("Enter your second number")
+        var secondNumber = reader.nextInt()
+
+        var result = firstNumber * secondNumber
+        println("Your multiplied result is $result")
+    }
+}
+
+fun main(){
+    val obj = Multiplication()
+    obj.multi()
 }
